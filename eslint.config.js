2d2e70d02a2c33c0c8badc319366293d29358ad9
灿烂 <1481086138@qq.com>
2025-06-08 22:55:41 +0800

    feat: 初始化项目eslint、prettier、editorConfig配置

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import js from "@eslint/js"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+import pluginVue from "eslint-plugin-vue"
+import { defineConfig } from "eslint/config"
+import prettierRecommended from "eslint-plugin-prettier/recommended"
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
+    plugins: { js },
+    extends: ["js/recommended"]
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } }
+  },
+  tseslint.configs.recommended,
+  pluginVue.configs["flat/essential"],
+  {
+    files: ["**/*.vue"],
+    languageOptions: { parserOptions: { parser: tseslint.parser } }
+  },
+  { ignores: [".css", "*.d.ts"] },
+  {
+    rules: {
+      "no-console": "warn"
+    }
+  },
+  prettierRecommended // 覆盖eslint的默认配置
+])
