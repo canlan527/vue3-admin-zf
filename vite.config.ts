07182bda970c68b9bf52657006e5623c27ff9af3
灿烂 <1481086138@qq.com>
2025-06-10 16:38:03 +0800

    feat: 配置@别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src')
+      }
+    ]
+  },
   plugins: [vue()]
 })
