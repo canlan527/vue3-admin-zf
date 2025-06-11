61ef541621812f98ac3cf288c9ad221043c7a527
灿烂 <1481086138@qq.com>
2025-06-11 21:40:51 +0800

    feat: 配置unocss icon图标方式一

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,16 +1,10 @@
 <template>
   <div>
-    dashboard
-    <el-button type="primary" @click="handleButton">点击</el-button>
+    <p>使用icon</p>
+    <div i-ant-design:apple-filled icons></div>
   </div>
 </template>
 
-<script setup lang="ts">
-// 使用element-plus的服务型组件示例
-const { proxy } = getCurrentInstance()!
-const handleButton = () => {
-  proxy?.$message('提交成功')
-}
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
--- uno.config.ts
+++ uno.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig, presetAttributify, presetWind3 } from 'unocss'
 import transformerDirectives from '@unocss/transformer-directives'
+import presetIcons from '@unocss/preset-icons'
 
 export default defineConfig({
   presets: [
     presetAttributify(),
-    presetWind3()
+    presetWind3(),
+    presetIcons()
     // ...custom presets
   ],
-  transformers: [transformerDirectives()]
+  transformers: [transformerDirectives()],
+  shortcuts: [['icons', 'inline-block w-1em h-1em align-middle text-current']]
 })
